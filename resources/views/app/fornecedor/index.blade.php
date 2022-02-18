0917fb850687647c54326e6ddb055e2afda7d2c1
Elton Andrade <eltonsa75@hotmail.com>
2022-02-18 09:41:00 -0300

    Criando @foreach no blade.php

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -24,18 +24,16 @@
 
 @isset($fornecedores)
 
-    @php $i = 0 @endphp
-    @while(isset($fornecedores[$i]))
-    Fornecedor: {{ $fornecedores[$i]['nome'] }}
+    @foreach ($fornecedores as $indices => $fornecedor)
+    Fornecedor: {{ $fornecedor['nome'] }}
     <br>
-    Status: {{ $fornecedores[$i]['status'] }}
+    Status: {{ $fornecedor['status'] }}
     <br>
-    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+    CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
     <br>
-    Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+    Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
     <hr>
-    @php $i++ @endphp
- @endwhile
+@endforeach
 @endisset
   {{--   @switch($fornecedores[0]['ddd'])
 
