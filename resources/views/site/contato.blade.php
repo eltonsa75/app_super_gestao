6c6bdb82fdeb6b0f7721785ccf2f5640ac8c2262
Elton Andrade <eltonsa75@hotmail.com>
2022-03-09 23:11:45 -0300

    Blade- Enviando parâmentros para componentes.

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -2,9 +2,7 @@
 
 @section('titulo', 'Contato')
 
-@section('conteudo')
-
-    
+@section('conteudo')    
 
         <div class="conteudo-pagina">
             <div class="titulo-pagina">
@@ -13,25 +11,10 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    <form action={{ route('site.contato') }} method="post">
-                    @csrf
-                        <input name="nome" type="text" placeholder="Nome" class="borda-preta">
-                        <br>
-                        <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
-                        <br>
-                        <input name="email" type="text" placeholder="E-mail" class="borda-preta">
-                        <br>
-                        <select name="motivo_contato" class="borda-preta">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="1">Dúvida</option>
-                            <option value="2">Elogio</option>
-                            <option value="3">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-preta">ENVIAR</button>
-                    </form>
+            @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                <p>A nossa equipe analisará a sua mensagem e retornaremos o mais brevemente possível</p>
+                <p>Nosso tempo médio de resposta é de 48 horas.</p>
+            @endcomponent
                 </div>
             </div>  
         </div>
