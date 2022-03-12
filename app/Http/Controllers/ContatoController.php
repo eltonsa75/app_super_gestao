785acce9aec3b96ec4700a087eac3547ae06b2c6
Elton Andrade <eltonsa75@hotmail.com>
2022-03-12 20:16:25 -0300

    Salvando os registro no banco com save()

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function Contato(){
-        var_dump($_POST);
+    public function contato(Request $request){
+
+    //   echo'<pre>';
+    //    print_r($request->all());
+    //    echo '</pre>';
+
+    //    echo $request->input('nome');
+    //    echo '<br>';
+    //    echo $request->input('email');
+    
+    $contato = new SiteContato();
+    
+    $contato->nome = $request->input('nome');
+    $contato->telefone = $request->input('telefone');
+    $contato->email = $request->input('email');
+    $contato->motivo_contato = $request->input('motivo_contato');
+    $contato->mensagem = $request->input('mensagem');
+
+    $contato->save();
+
+
         return view('site.contato');
     }
 }
