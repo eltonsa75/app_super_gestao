82a10996be94cf808be9be90b8ad258be2c18dbf
Elton Andrade <eltonsa75@hotmail.com>
2022-03-13 21:15:46 -0300

    Buscando o Option direto do banco de dados.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,36 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request){
 
-    //   echo'<pre>';
-    //    print_r($request->all());
-    //    echo '</pre>';
+        $motivo_contatos = MotivoContato::all();
 
-    //    echo $request->input('nome');
-    //    echo '<br>';
-    //    echo $request->input('email');
-    
-    // $contato = new SiteContato();
-    
-    // $contato->nome = $request->input('nome');
-    // $contato->telefone = $request->input('telefone');
-    // $contato->email = $request->input('email');
-    // $contato->motivo_contato = $request->input('motivo_contato');
-    // $contato->mensagem = $request->input('mensagem');
-
-    // $contato->save();
-
-    //$contato = new SiteContato();
-    //$contato->fill($request->all());
-    //$contato->create($request->all());
-    //$contato->save();
-    //print_r($contato->getAttributes());
-
-        return view('site.contato');
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
