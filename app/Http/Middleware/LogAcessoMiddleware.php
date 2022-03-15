70311f7c2ea49f32e627f9ee4ecc6562146ec955
Elton Andrade <eltonsa75@hotmail.com>
2022-03-15 00:04:38 -0300

    Implementando Middleware no sistema de Gestão

---
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+use Closure;
+use App\LogAcesso;
+
+class LogAcessoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //$request - manipular
+        //return $next($request);       
+
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
+        return Response('Chegando no middleware e finalizando no próprio middleware');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)
+->get('/', 'PrincipalController@principal')
+->name('site.index');
+
 Route::get('/sobre-nos', 'SobrenosController@sobrenos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::middleware(LogAcessoMiddleware::class)
+->get('/contato', 'ContatoController@contato')
+->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
 Route::get('/login', function(){return 'Login'; })->name('site.login');
