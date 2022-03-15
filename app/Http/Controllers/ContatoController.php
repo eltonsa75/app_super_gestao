b4cfd420d7c21029c7a29ee6ed46ff39c5253769
Elton Andrade <eltonsa75@hotmail.com>
2022-03-14 23:27:03 -0300

    Criando mensagem de errors

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,13 +17,32 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
         //realizar as validação dos dados do formuláro recebido no request
-        $request->validate([
+
+        $regras = 
+        [
             'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000',
-        ]);
+        ];
+
+        $feedback =
+        [
+            
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+            
+            'email.email' => 'O email informado não é válido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+
+        ];
+        
+        $request->validate($regras, $feedback);
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
