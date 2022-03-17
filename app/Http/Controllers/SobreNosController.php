8eabed49ca3c19ab7ccfa53e69a32e9a5128796e
Elton Andrade <eltonsa75@hotmail.com>
2022-03-17 13:49:12 -0300

    Middleware para todas API Web

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,15 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
-    
-    public function __construct(){
-        $this->middleware(LogAcessoMiddleware::class);
-    }
-    
+ 
     public function SobreNos(){
         return view('site.sobre-nos');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\LogAcessoMiddleware;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +15,11 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 
-Route::middleware(LogAcessoMiddleware::class)
-->get('/', 'PrincipalController@principal')
-->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
 Route::get('/sobre-nos', 'SobrenosController@sobrenos')->name('site.sobrenos');
 
-Route::middleware(LogAcessoMiddleware::class)
-->get('/contato', 'ContatoController@contato')
-->name('site.contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
 Route::get('/login', function(){return 'Login'; })->name('site.login');
