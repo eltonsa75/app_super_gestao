b684a48f07cd205ccff85a8691e1c441a49f36ba
Elton Andrade <eltonsa75@hotmail.com>
2022-03-18 18:37:01 -0300

    Formulario fornecedor

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
     }
 }
