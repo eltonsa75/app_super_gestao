b38f2c0d0b341788a2ea48df5e75b45c0fceef67
Elton Andrade <eltonsa75@hotmail.com>
2022-03-18 11:43:05 -0300

    Enviando parâmetros via route e get

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,14 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
-    public function index(){
-        return view('site.login', ['titulo' => 'Login']);
+    public function index(Request $request){
+
+        $erro = '';
+        
+        if($request->get('erro') == 1){
+            $erro = 'Usuário ou senha não existe';
+        }
+
+        return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
     }
 
-    public function autenticar(){
-        return 'Chegamos até aqui!';
+    public function autenticar(Request $request){
+
+        //regra de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //as mensagem de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (email) é obrigatório',
+            'senha.required' => 'O campo (senha) é obrigatório'
+        ];
+
+        $request->validate($regras, $feedback);
+        
+
+        //recuperamos os parâmentros do formulário
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuário: $email | Senha: $password";
+        echo '<br>';
+
+
+        //iniciar o Model User
+        $user = new User();
+
+        $usuario = $user->where('email', $email)
+        ->where('password', $password)
+        ->get()
+        ->first();
+
+        if(isset($usuario->name)){
+            echo 'Usuário existe';
+        } else {
+            return redirect()->route('site.login', ['erro' => 1]);
+        }
+
+
+        
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/sobre-nos', 'SobrenosController@sobrenos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
-Route::get('/login', 'LoginController@index')->name('site.login');
+Route::get('/login/{erro?}', 'LoginController@index')->name('site.login');
 Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 // Agrupamento de rotas
