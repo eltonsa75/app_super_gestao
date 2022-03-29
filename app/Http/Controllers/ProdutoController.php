bc026ecfdd73bb307495d255bb2547f15b73498a
Elton Andrade <eltonsa75@hotmail.com>
2022-03-29 15:30:22 -0300

    Criando as validações e os feedback direto na ProdutoController.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -40,7 +40,27 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:2000',
+            'peso' => 'required|integer',
+            'unidade_id' => 'exists:unidades,id'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'nome.min' => 'O campo nome deve ter no minimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'descricao.min' => 'O campo descrição deve ter no mínimo 3 caracteres',
+            'descricao.max' => 'O campo descrição deve ter no máximo 2000 caracteres',
+            'peso.integer' => 'O campo peso deve ser um numero inteiro',
+            'unidade_id.exists' => 'A unidade de medida informada não existe'
+        ];
+
+        $request->validate($regras, $feedback);
+        
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
     }
 
     /**
--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -20,26 +20,26 @@
         <div class="informacao-pagina">
      
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="">
+                <form method="post" action="{{ route('produto.store') }}">
                     
                 @csrf
-                    <input type="text" name="nome" value="" placeholder="Nome" class="borda-preta">
-                   
+                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
+                    {{ $errors->has('nome') ? $errors->first('nome') : ''}}
 
-                    <input type="text" name="descricao" value="" placeholder="Descrição" class="borda-preta">
-                    
+                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descrição" class="borda-preta">
+                    {{ $errors->has('descricao') ? $errors->first('descricao') : ''}}
 
-                    <input type="text" name="peso" value="" placeholder="Peso" class="borda-preta">
-                    
+                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="Peso" class="borda-preta">
+                    {{ $errors->has('peso') ? $errors->first('peso') : ''}}
 
                     <select name="unidade_id">
                     <option>-- Selecione a Unidade de Medida --</option>
 
                     @foreach ($unidades as $unidade)
-                    <option value="{{ $unidade->id }}">{{ $unidade->descricao }}Unidade</option>
+                    <option value="{{ $unidade->id }}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}Unidade</option>
                     @endforeach
                     </select>
-                    
+                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : ''}}
 
                     <button type="submit" class="borda-preta">Cadastrar</button>                   
                 </form>
