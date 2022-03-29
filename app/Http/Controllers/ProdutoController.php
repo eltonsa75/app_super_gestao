f8c0348ed26e62041dd23e15329b9f1ba496104c
Elton Andrade <eltonsa75@hotmail.com>
2022-03-29 20:00:07 -0300

    Criando o método edit na controllerProduto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -82,7 +82,8 @@ class ProdutoController extends Controller
      */
     public function edit(Produto $produto)
     {
-        //
+        $unidades = Unidade::all();
+        return view('app.produto.edit', ['produto' => $produto, 'unidades' => $unidades]);
     }
 
     /**
--- resources/views/app/produto/edit.blade.php
+++ resources/views/app/produto/edit.blade.php
@@ -7,7 +7,7 @@
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
-            <p> Adicionar - Produto</p>
+            <p> Editar - Produto</p>
         </div>
 
         <div class="menu">
@@ -20,23 +20,23 @@
         <div class="informacao-pagina">
      
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="{{ route('produto.store') }}">
+                <form method="post" action="">
                     
                 @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
+                    <input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
                     {{ $errors->has('nome') ? $errors->first('nome') : ''}}
 
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descrição" class="borda-preta">
+                    <input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" placeholder="Descrição" class="borda-preta">
                     {{ $errors->has('descricao') ? $errors->first('descricao') : ''}}
 
-                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="Peso" class="borda-preta">
+                    <input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" placeholder="Peso" class="borda-preta">
                     {{ $errors->has('peso') ? $errors->first('peso') : ''}}
 
                     <select name="unidade_id">
                     <option>-- Selecione a Unidade de Medida --</option>
 
                     @foreach ($unidades as $unidade)
-                    <option value="{{ $unidade->id }}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
+                    <option value="{{ $unidade->id }}" {{ $produto->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
                     @endforeach
                     </select>
                     {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : ''}}
