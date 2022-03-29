7e4402aa2daf4470a476051e376b9d3fe9098fb2
Elton Andrade <eltonsa75@hotmail.com>
2022-03-29 20:18:06 -0300

    Criando o método update de registro.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -95,7 +95,8 @@ class ProdutoController extends Controller
      */
     public function update(Request $request, Produto $produto)
     {
-        //
+        $produto->update($request->all());
+        return redirect()->route('produto.show', ['produto' => $produto->id]);
     }
 
     /**
