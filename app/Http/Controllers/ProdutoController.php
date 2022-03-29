adb98baacde4be66647b71be69d8c745b7133033
Elton Andrade <eltonsa75@hotmail.com>
2022-03-29 20:31:39 -0300

    Criando o método DELETE  do projeto na controller

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -107,6 +107,7 @@ class ProdutoController extends Controller
      */
     public function destroy(Produto $produto)
     {
-        //
+        $produto->delete();
+        return redirect()->route('produto.index');
     }
 }
