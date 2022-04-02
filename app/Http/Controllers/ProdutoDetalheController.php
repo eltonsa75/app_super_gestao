2e32d2a4c89786129110df4f9d1600b11e604950
Elton Andrade <eltonsa75@hotmail.com>
2022-04-02 01:36:27 -0300

    Eager Loading

--- app/Http/Controllers/ProdutoDetalheController.php
+++ app/Http/Controllers/ProdutoDetalheController.php
@@ -61,7 +61,7 @@ class ProdutoDetalheController extends Controller
      */
     public function edit($id)
     {
-        $produtoDetalhe = ItemDetalhe::find($id);
+        $produtoDetalhe = ItemDetalhe::with(['item'])->find($id);
         $unidades = Unidade::all();
         return view('app.produto_detalhe.edit', ['produto_detalhe' => $produtoDetalhe, 'unidades' => $unidades]);
     }
