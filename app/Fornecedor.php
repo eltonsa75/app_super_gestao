ec607b787407a25e3b048fb66359b67a83ba2124
Elton Andrade <eltonsa75@hotmail.com>
2022-04-11 16:25:34 -0300

    Exibindo o nome do Foencedor na View

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -16,4 +16,10 @@ class Fornecedor extends Model
     //Utilizado para alterar o nome da tabela
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+    public function produtos(){
+       return $this->hasMany('App\Item', 'fornecedor_id', 'id');
+       //return $this->hasMany('App\Item');
+       
+    }
 }
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -24,7 +24,8 @@
                         <tr>
                             <th>Nome</th>
                             <th>Descrição</th>
-                            <th>fornecedor</th>
+                            <th>Fornecedor</th>
+                            <th>Site Fornecedor</th>
                             <th>Peso</th>
                             <th>Unidade ID</th>
                             <th>Comprimento</th>
@@ -39,6 +40,7 @@
                                 <td>{{ $produto->nome }}</td>
                                 <td>{{ $produto->descricao }}</td>
                                 <td>{{ $produto->fornecedor->nome }}</td>
+                                <td>{{ $produto->fornecedor->site }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
                                 <td>{{ $produto->itemDetalhe->comprimento ?? '' }}</td>
