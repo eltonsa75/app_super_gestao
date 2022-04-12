61721482b8387a0e8e72add14691a060b1b61f77
Elton Andrade <eltonsa75@hotmail.com>
2022-04-12 10:35:34 -0300

    Criando tela de cadastro e pedido.

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pedido;
 
 class PedidoController extends Controller
 {
@@ -11,9 +12,10 @@ class PedidoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $pedidos = Pedido::paginate(10);
+        return view('app.pedido.index', ['pedidos' => $pedidos, 'request' => $request->all()]);
     }
 
     /**
